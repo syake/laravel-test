5a8d496e570fc736bc9d74d047e3aec0269a95d9
Hiroaki Komatsu <komatsu@syake-labo.com>
2018-08-03 21:34:48 +0900

    ログイン後のHOMEを追加

---
+++ www/resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <p>You are logged in!</p>
+                    <ul class="list-group">
+                        <li class="list-group-item"><a href="/posts">{{ __('Posts') }}</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
